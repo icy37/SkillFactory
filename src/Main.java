cb6624c680ba48829f1493f407417c7654daedea
Maxim <icy375@gmail.com>
2023-04-27 19:08:43 +0300

    Test_conflict_branch_2_megrate

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Lesson_5.Squirrel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Branch2");
+        System.out.println("Branch_2");
 
     }
 }
